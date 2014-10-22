160881c97be73ba2c3470fef86bfbe5488b41e91
blumen <johan.blumenberg@gmail.com>
2014-10-22 22:18:08 +0200

    Working project

--- levon/gen/com/example/levon/R.java
+++ levon/gen/com/example/levon/R.java
@@ -1537,6 +1537,15 @@ containing a value of this type.
  Default text size for action bar title.
          */
         public static final int abc_text_size_title_material_toolbar=0x7f070012;
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+         */
+        public static final int activity_horizontal_margin=0x7f07002b;
+        public static final int activity_vertical_margin=0x7f07002c;
         /**  The platform's desired fixed height for a dialog along the major axis
          (the screen is in portrait). This may be either a fraction or a dimension.
  The platform's desired fixed height for a dialog along the major axis
@@ -1640,10 +1649,13 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f090030;
         public static final int action_mode_bar_stub=0x7f09002f;
         public static final int action_mode_close_button=0x7f090022;
+        public static final int action_settings=0x7f090044;
         public static final int activity_chooser_view_content=0x7f090023;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
         public static final int bottom=0x7f09001e;
+        public static final int btn_listen=0x7f090041;
+        public static final int btn_send=0x7f090042;
         public static final int checkbox=0x7f09002b;
         public static final int collapseActionView=0x7f090015;
         public static final int decor_content_parent=0x7f090031;
@@ -1688,6 +1700,7 @@ containing a value of this type.
         public static final int tabMode=0x7f09000c;
         public static final int title=0x7f090029;
         public static final int top=0x7f09001f;
+        public static final int txt_result=0x7f090043;
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
         public static final int withText=0x7f090018;
@@ -1745,6 +1758,9 @@ containing a value of this type.
         public static final int activity_main=0x7f030017;
         public static final int support_simple_spinner_dropdown_item=0x7f030018;
     }
+    public static final class menu {
+        public static final int main=0x7f0c0000;
+    }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
@@ -1799,8 +1815,10 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000d;
+        public static final int action_settings=0x7f0a0012;
         public static final int app_name=0x7f0a000f;
-        public static final int hello_world=0x7f0a0010;
+        public static final int btn_listen=0x7f0a0010;
+        public static final int btn_send=0x7f0a0011;
     }
     public static final class style {
         /** 
