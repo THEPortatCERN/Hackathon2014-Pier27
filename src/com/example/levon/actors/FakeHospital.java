962c402ecc5e5e637b62a96a660aacd61e9049c3
blumen <johan.blumenberg@gmail.com>
2014-10-28 19:56:41 +0100

    Working fake private key

--- src/com/example/levon/actors/FakeHospital.java
+++ src/com/example/levon/actors/FakeHospital.java
@@ -4,28 +4,30 @@ import com.example.levon.utils.SignUtils;
 import com.example.levon.utils.SignedMessage;
 
 public class FakeHospital {
-	private static final String message = "";
+	private static final String message = "Hospital name: Red Cross\nLocation: 0°,0°\nSigned by: safezone.org";
 
 	//
 	// Fake private key, generated using:
 	//
-	// $ openssl genrsa -out fake_key.pem 1024
+	// $ openssl genrsa -out fake.key 1024
+	// $ openssl pkcs8 -topk8 -inform pem -in fake.key -outform pem -nocrypt
+	// -out fake.pk8
 	//
-	private static final String PRIVATE_KEY = "-----BEGIN RSA PRIVATE KEY-----\n"
-			+ "MIICWwIBAAKBgQDAeZyorYQaGKZzFO1Y6WIoGqoBqCCiXspjhxew9H3+cCKtff6k\n"
-			+ "7zkZuqkrKvqvR8HYMthwDQahpBrZIefSz9F42ROYNgGTZj1zJm2L3jZybX2oqCcW\n"
-			+ "3ojVCwjMfKASSPZXBWcWy5MUHuyh8hYFuzcDXGFIiplv1mF9S6Bp72Bv0wIDAQAB\n"
-			+ "AoGAUoMP7KPOEN3mZUlgYpBSWvzrSqCHtPPL8ZTVTC/T34K06wkrZAuFwAjsQqw/\n"
-			+ "IWsyp3tMm+SJpjvSoqjNV+ttUZGc5J1yHpuXKFGL+GfPtk2uBc9rl7yZHqjl69Az\n"
-			+ "/fonThyYg4jpxr24hqlpGKeZ6xdFq2SL+5cmT4GKjoBCR7kCQQDrnu2mG1ElN0cF\n"
-			+ "UAVAhtCSGcz9MPRpDTtXxumK4nJrhnovhyQmYGiDkMmLsXvJ1PjRQFVOI3GDukhx\n"
-			+ "eQ+unWVPAkEA0R9bsfZZmffJE32Hmh9J9/+j9mVDLZvvstU6rpsPrSHKAtYmvACV\n"
-			+ "zCaZytqlsQd6+6woBQnlsSwWyxBTh7D0PQJARklsnyZ6+F0bBIywZGVTBqJHsifO\n"
-			+ "YSAfWbnupQ2UEFtXS6JGuGP+CXFBjjhtsvGjH9jPGbYdAKyrO55ffTlapQJAeLV2\n"
-			+ "E0pUvAgIG+T4keD/rFP2i7I1zazV9k8YeVLuAIyhc3Ozlg9saBPb7/i3bXEY7w6X\n"
-			+ "ZxkoaVU+ruX0oT1mLQJAJfpyu6i1f2UcQuqCpbBRMsnhXglyexoBPxYQuGt4ohQG\n"
-			+ "7I0vKuSKPD0caJpz9YzbFKNvPHsmTDY7iylZoaqUJg==\n"
-			+ "-----END RSA PRIVATE KEY-----\n";
+	private static final String PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\n"
+			+ "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAMB5nKithBoYpnMU\n"
+			+ "7VjpYigaqgGoIKJeymOHF7D0ff5wIq19/qTvORm6qSsq+q9Hwdgy2HANBqGkGtkh\n"
+			+ "59LP0XjZE5g2AZNmPXMmbYveNnJtfaioJxbeiNULCMx8oBJI9lcFZxbLkxQe7KHy\n"
+			+ "FgW7NwNcYUiKmW/WYX1LoGnvYG/TAgMBAAECgYBSgw/so84Q3eZlSWBikFJa/OtK\n"
+			+ "oIe088vxlNVML9PfgrTrCStkC4XACOxCrD8hazKne0yb5ImmO9KiqM1X621RkZzk\n"
+			+ "nXIem5coUYv4Z8+2Ta4Fz2uXvJkeqOXr0DP9+idOHJiDiOnGvbiGqWkYp5nrF0Wr\n"
+			+ "ZIv7lyZPgYqOgEJHuQJBAOue7aYbUSU3RwVQBUCG0JIZzP0w9GkNO1fG6YricmuG\n"
+			+ "ei+HJCZgaIOQyYuxe8nU+NFAVU4jcYO6SHF5D66dZU8CQQDRH1ux9lmZ98kTfYea\n"
+			+ "H0n3/6P2ZUMtm++y1Tqumw+tIcoC1ia8AJXMJpnK2qWxB3r7rCgFCeWxLBbLEFOH\n"
+			+ "sPQ9AkBGSWyfJnr4XRsEjLBkZVMGokeyJ85hIB9Zue6lDZQQW1dLoka4Y/4JcUGO\n"
+			+ "OG2y8aMf2M8Zth0ArKs7nl99OVqlAkB4tXYTSlS8CAgb5PiR4P+sU/aLsjXNrNX2\n"
+			+ "Txh5Uu4AjKFzc7OWD2xoE9vv+LdtcRjvDpdnGShpVT6u5fShPWYtAkAl+nK7qLV/\n"
+			+ "ZRxC6oKlsFEyyeFeCXJ7GgE/FhC4a3iiFAbsjS8q5Io8PRxomnP1jNsUo288eyZM\n"
+			+ "NjuLKVmhqpQm\n" + "-----END PRIVATE KEY-----\n";
 
 	//
 	// Take the signed hospital message and change the message
